4545de3c4543235ab8675111fa0a8f6d2248555e
Noel Daniel T. Seldura <daniel.seldura@gmail.com>
2022-12-01 15:03:35 +0800

    Updated customtext

--- lib/custom_text.dart
+++ lib/custom_text.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 
-import '../styles.dart';
-import 'services/navigation/navigation_service.dart';
-
 enum TextType {
   h1,
+  h1ExtraLight,
   h1Bold,
   h2Bold,
+  h2Light,
   h2Heavy,
   h3,
   h3Bold,
@@ -16,8 +15,11 @@ enum TextType {
   h4Bold,
   h4Heavy,
   h5,
+  h5Light,
+  h5Thin,
   h5Bold,
   h5SemiBold,
+  h5Heavy,
   body,
   caption,
   captionBold,
@@ -35,7 +37,7 @@ class CustomText extends StatelessWidget {
   final String text;
   final double? sizeOverride;
   final TextType textType;
-  final Color color;
+  final Color? color;
   final EdgeInsets? margin;
   final TextAlign? textAlign;
   final FontStyle fontStyle;
@@ -45,9 +47,10 @@ class CustomText extends StatelessWidget {
   final double? letterSpacing;
   final TextDecoration? decoration;
   final bool selectable;
-  CustomText(this.text,
+
+  const CustomText(this.text,
       {this.textType = TextType.h5,
-      Color? color,
+      this.color,
       this.fontFamily,
       this.letterSpacing,
       this.margin = EdgeInsets.zero,
@@ -60,20 +63,23 @@ class CustomText extends StatelessWidget {
       this.sizeOverride,
       this.selectable = false,
       Key? key})
-      : color = color ?? AppStyleCore.black,
-        super(key: key);
+      : super(key: key);
 
-  static TextStyle? style(TextType textType, {Color? color}) {
-    TextTheme textTheme =
-        Theme.of(NavigationService.instance.navigatorKey.currentContext!)
-            .textTheme;
+  static TextStyle? style(TextType textType, {BuildContext? context}) {
+    TextTheme textTheme = context == null
+        ? ThemeData.dark().textTheme
+        : Theme.of(context).textTheme;
     switch (textType) {
       case TextType.h1:
         return textTheme.headline1;
+      case TextType.h1ExtraLight:
+        return textTheme.headline1?.copyWith(fontWeight: FontWeight.w200);
       case TextType.h1Bold:
         return textTheme.headline1?.copyWith(fontWeight: FontWeight.w800);
       case TextType.h2Bold:
         return textTheme.headline2?.copyWith(fontWeight: FontWeight.w800);
+      case TextType.h2Light:
+        return textTheme.headline2?.copyWith(fontWeight: FontWeight.w300);
       case TextType.h2Heavy:
         return textTheme.headline2?.copyWith(fontWeight: FontWeight.w900);
       case TextType.h3:
@@ -92,27 +98,41 @@ class CustomText extends StatelessWidget {
         return textTheme.headline4?.copyWith(fontWeight: FontWeight.w900);
       case TextType.h4SemiBold:
         return textTheme.headline4?.copyWith(fontWeight: FontWeight.w700);
-      case TextType.h5:
-        return textTheme.headline5;
-      case TextType.h5Bold:
+
+      case TextType.h5Heavy:
         return textTheme.headline5?.copyWith(
           fontWeight: FontWeight.w900,
         );
+      case TextType.h5Bold:
+        return textTheme.headline5?.copyWith(
+          fontWeight: FontWeight.w700,
+        );
       case TextType.h5SemiBold:
         return textTheme.headline5?.copyWith(
-          fontWeight: FontWeight.w800,
+          fontWeight: FontWeight.w600,
         );
+      case TextType.h5:
+        return textTheme.headline5;
+
+      case TextType.h5Thin:
+        return textTheme.headline5?.copyWith(fontWeight: FontWeight.w300);
+
+      case TextType.h5Light:
+        return textTheme.headline5?.copyWith(fontWeight: FontWeight.w100);
+
       case TextType.body:
         return textTheme.bodyText1;
       case TextType.caption:
-        return textTheme.caption;
-      case TextType.captionBold:
-        return textTheme.caption?.copyWith(fontWeight: FontWeight.w600);
-      case TextType.captionMini:
         return textTheme.caption?.copyWith(fontSize: 12);
+      case TextType.captionBold:
+        return textTheme.caption
+            ?.copyWith(fontSize: 12, fontWeight: FontWeight.w600);
+      case TextType.captionMini:
+        return textTheme.caption
+            ?.copyWith(fontSize: 10, fontWeight: FontWeight.w300);
       case TextType.captionMiniBold:
         return textTheme.caption
-            ?.copyWith(fontSize: 12, fontWeight: FontWeight.w700);
+            ?.copyWith(fontSize: 10, fontWeight: FontWeight.w700);
       case TextType.newT:
         return textTheme.bodyText1
             ?.copyWith(fontSize: 13.5, fontWeight: FontWeight.w300);
@@ -131,40 +151,35 @@ class CustomText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    TextStyle ctStyle = style(textType) as TextStyle;
+    TextStyle ctStyle = style(textType, context: context) as TextStyle;
+    if (selectable) {
+      return SelectableRegion(
+          focusNode: FocusNode(),
+          selectionControls: materialTextSelectionControls,
+          child: buildContent(ctStyle));
+    } else {
+      return buildContent(ctStyle);
+    }
+  }
 
+  Container buildContent(TextStyle ctStyle) {
     return Container(
       margin: margin,
-      child: selectable
-          ? SelectableText(
-              text,
-              style: ctStyle.copyWith(
-                  letterSpacing: letterSpacing,
-                  fontStyle: fontStyle,
-                  color: color,
-                  height: lineSpacing ?? 1.0,
-                  fontFamily: fontFamily,
-                  fontFamilyFallback: ['Roboto'],
-                  decoration: decoration,
-                  fontSize: sizeOverride),
-              textAlign: textAlign,
-              maxLines: maxLines,
-            )
-          : Text(
-              text,
-              style: ctStyle.copyWith(
-                  letterSpacing: letterSpacing,
-                  fontStyle: fontStyle,
-                  color: color,
-                  height: lineSpacing ?? 1.0,
-                  fontFamily: fontFamily,
-                  fontFamilyFallback: ['Roboto'],
-                  decoration: decoration,
-                  fontSize: sizeOverride),
-              textAlign: textAlign,
-              maxLines: maxLines,
-              overflow: overflow,
-            ),
+      child: Text(
+        text,
+        style: ctStyle.copyWith(
+            letterSpacing: letterSpacing,
+            fontStyle: fontStyle,
+            color: color ?? ctStyle.color,
+            height: lineSpacing ?? 1.0,
+            fontFamily: fontFamily,
+            fontFamilyFallback: ['Roboto'],
+            decoration: decoration,
+            fontSize: sizeOverride),
+        textAlign: textAlign,
+        maxLines: maxLines,
+        overflow: overflow,
+      ),
     );
   }
 }
